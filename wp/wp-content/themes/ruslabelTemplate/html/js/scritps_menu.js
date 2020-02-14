720cca15325b24327004d170e00fb2e330357229
dark37rus <dark.1996rus@gmail.com>
2020-02-14 18:10:55 +0300

    Добавление меню на сайте на для мобильной так и для компьютерной версии

--- wp/wp-content/themes/ruslabelTemplate/html/js/scritps_menu.js
+++ wp/wp-content/themes/ruslabelTemplate/html/js/scritps_menu.js
@@ -1,9 +1,29 @@
-jQuery(document).ready(function($){
-        var $preloader = $('#p_prldr');
-        function preLoader () {
-            $('#p_prldr').fadeOut(1000);
-        }
-        setTimeout(preLoader, 1300);
+$(document).ready(function() {
+
+    function preLoader () {
+        $('#p_prldr').fadeOut(1000);
+    }
+    setTimeout(preLoader, 1000);
+    $('.submenu').attr('data-visibal', 'false');
+
+    $('.menu__dropdown').on('click', function() {
+        $x = $(this).next('.submenu');
+        $x.toggle(500);
+
+        $y = $('ul.menu > li > ul li .submenu').not($x);
+        $y.fadeOut(500);
+    });
+
+
+
+    $('#menu_swither').click(function () {
+        $('#menu_swither > .fa-bars').toggle(10);
+        $('#menu_swither > .fa-times').toggle(10);
+        $('.menu_line').toggle(500);
+    });
 });
+
+
+
 
 
